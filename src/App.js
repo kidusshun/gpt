3a885a237b804adbfb672710269c0286c056814f
unknown <kidusshun@gmail.com>
2023-03-02 14:52:51 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Blog, Footer, Features, Header, Possibility, WhatGPT } from "./container/index";
+import { Brand, CTA, Navbar } from "./components";
+
+import './App.css'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
