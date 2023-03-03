956cde6df80b9b8d87337186b1f566492de56796
kidusshun <kidusshun@gmail.com>
2023-03-03 23:50:19 +0300

    footer and blog done

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import './Blog.css'
+import { Article } from '../../components';
+import './Blog.css';
+import blog01 from '../../assets/blog01.png';
+import blog02 from '../../assets/blog02.png';
+import blog03 from '../../assets/blog03.png';
+import blog04 from '../../assets/blog04.png';
+import blog05 from '../../assets/blog05.png';
 
 const Blog = () => {
   return (
@@ -10,11 +16,16 @@ const Blog = () => {
           We are blogging about it.
         </h1>
       </div>
-      <div className='gpt__blog-container'>
+      <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          A
+          <Article imageUrl={blog01} date="sep 26,2022" title="GPT-3 and Open AI is the future. Let us explore how it is." />
         </div>
-        <div className='gpt3__blog-container_groupB'></div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imageUrl={blog02} date="sep 26,2022" title="GPT-3 and Open AI is the future. Let us explore how it is." />
+          <Article imageUrl={blog03} date="sep 26,2022" title="GPT-3 and Open AI is the future. Let us explore how it is." />
+          <Article imageUrl={blog04} date="sep 26,2022" title="GPT-3 and Open AI is the future. Let us explore how it is." />
+          <Article imageUrl={blog05} date="sep 26,2022" title="GPT-3 and Open AI is the future. Let us explore how it is." />
+        </div>
       </div>
     </div>
   )
