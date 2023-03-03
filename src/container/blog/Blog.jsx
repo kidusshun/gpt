25ed89b802b7ee5b162e707f2db53625536eedd8
kidusshun <kidusshun@gmail.com>
2023-03-03 22:15:08 +0300

    cta done

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -3,7 +3,20 @@ import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>
+          A lot is happening,<br />
+          We are blogging about it.
+        </h1>
+      </div>
+      <div className='gpt__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          A
+        </div>
+        <div className='gpt3__blog-container_groupB'></div>
+      </div>
+    </div>
   )
 }
 
